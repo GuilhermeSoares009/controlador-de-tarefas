947a33b50b4f763fa2f19c1caf554b843f5c575e
Guilherme Soares <guilherme.sousa009@hotmail.com>
2023-07-17 18:31:02 -0300

    Modificando home para redirecionar para tarefas + Atualizando dados de cada tarefa

--- app/Http/Controllers/TarefaController.php
+++ app/Http/Controllers/TarefaController.php
@@ -86,7 +86,7 @@ class TarefaController extends Controller
      */
     public function edit(Tarefa $tarefa)
     {
-        //
+        return view('tarefas.edit', ['tarefa' => $tarefa]);
     }
 
     /**
@@ -98,7 +98,8 @@ class TarefaController extends Controller
      */
     public function update(Request $request, Tarefa $tarefa)
     {
-        //
+        $tarefa->update($request->all());
+        return redirect()->route('tarefa.show',['tarefa' => $tarefa->id]);
     }
 
     /**
